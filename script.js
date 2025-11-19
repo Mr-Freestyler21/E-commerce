f75c8e7b13f8c542c2389daab968ec8c15e50cfb
Mr-Freestyler21 <91937804+Mr-Freestyler21@users.noreply.github.com>
2025-11-19 12:09:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const lenis = new Lenis({
+        duration: 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        direction: 'vertical',
+        gestureDirection: 'vertical',
+        smooth: true,
+        mouseMultiplier: 1,
+        smoothTouch: false, 
+        touchMultiplier: 2,
+        infinite: false,
+    });
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            lenis.scrollTo(this.getAttribute('href'));
+        });
+    });
+
